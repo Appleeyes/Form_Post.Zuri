14dc19c4956288f74d9db3baf2e845fdb3909e02
Appleeyes <86308689+Appleeyes@users.noreply.github.com>
2022-05-28 19:10:56 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Post</title>
+</head>
+<body>
+   <form method="POST" action="user_data.php">
+      <label for="name">
+        Name:<input type="text" name="name">
+      </label>
+      
+        </br></br>
+        
+      <label for="email">      
+        Email:<input type="email" name="email">
+      </label>
+      
+        </br></br>
+      
+      <label for="d_o_b">
+        D_O_B:<input type="date" name="date">
+      </label>
+      
+        </br></br>
+        
+      <label for="gender">
+        Gender:<input type="text" name="gender">
+      </label>
+      
+        </br></br>
+        
+      <label for="country">
+        Country:<input type="text" name="country">
+      </label>
+      
+        </br></br>
+                       
+        <button type="submit">Submit</button>
+   </form>
+   
+</body>
+</html>
---
+++ user_data.php
@@ -0,0 +1,13 @@
+<?php
+ $name= $_POST["name"];
+ $email= $_POST["email"];
+ $D_O_B= $_POST["date"];
+ $gender= $_POST["gender"];
+ $country= $_POST["country"];
+ 
+ echo "NAME: " . $name . "<br>";
+ echo "EMAIL: " . $email . "<br>";
+ echo "D_O_B: " . $D_O_B . "<br>";
+ echo "GENDER: " . $gender . "<br>";
+ echo "COUNTRY: " . $country . "<br>";
+?>
